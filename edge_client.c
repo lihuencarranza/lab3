5e27a18dab25521f03e6e6ace9d8af455647e35e
Lihuen <lcarranza@fi.uba.ar>
2025-05-06 11:43:34 +0200

    add: edge_client layer

---
+++ edge_client.c
@@ -0,0 +1,75 @@
+// edge_client.c
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#define SENSOR_IP "127.0.0.1"   // IP de RPi-1 con el sensor
+#define SENSOR_PORT 5000
+
+#define ACTUATOR_IP "127.0.0.1" // IP de RPi-2 con el LED
+#define ACTUATOR_PORT 5001
+
+int read_humidity() {
+    int sock;
+    struct sockaddr_in address;
+    int humidity = -1;
+
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    address.sin_family = AF_INET;
+    address.sin_port = htons(SENSOR_PORT);
+    inet_pton(AF_INET, SENSOR_IP, &address.sin_addr);
+
+    if (connect(sock, (struct sockaddr*)&address, sizeof(address)) < 0) {
+        perror("Error conectando a sensor");
+        return -1;
+    }
+
+    recv(sock, &humidity, sizeof(humidity), 0);
+    close(sock);
+    return humidity;
+}
+
+void send_led_command(const char *cmd) {
+    int sock;
+    struct sockaddr_in address;
+
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    address.sin_family = AF_INET;
+    address.sin_port = htons(ACTUATOR_PORT);
+    inet_pton(AF_INET, ACTUATOR_IP, &address.sin_addr);
+
+    if (connect(sock, (struct sockaddr*)&address, sizeof(address)) < 0) {
+        perror("Error conectando a actuador");
+        return;
+    }
+
+    send(sock, cmd, strlen(cmd), 0);
+    close(sock);
+}
+
+int main() {
+    while (1) {
+        int humidity = read_humidity();
+        if (humidity < 0) {
+            sleep(1);
+            continue;
+        }
+
+        float voltage = humidity * 3.3 / 1023.0;
+        printf("Humedad (ADC): %d - Voltaje: %.2f V\n", humidity, voltage);
+
+        if (humidity < 30) {
+            printf("Humedad < 30 => Enviando comando ON al LED\n");
+            send_led_command("ON");
+        } else {
+            printf("Condici�n no cumplida. No se enciende el LED.\n");
+        }
+
+        sleep(5);
+    }
+
+    return 0;
+}
