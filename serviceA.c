79b4d94d53b57b4f7dd7206cade89dc943447188
Lihuen <lcarranza@fi.uba.ar>
2025-05-06 11:41:12 +0200

    add: rpi-1 (humidity sensor)

---
+++ serviceA.c
@@ -0,0 +1,77 @@
+// servicio_A.c
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <linux/spi/spidev.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+#define SPI_DEVICE "/dev/spidev0.0"
+#define SPI_SPEED 1350000
+#define PORT 5000
+
+int read_channel(int fd, uint8_t channel) {
+    uint8_t tx[] = {1, (8 + channel) << 4, 0};
+    uint8_t rx[3] = {0};
+
+    struct spi_ioc_transfer tr = {
+        .tx_buf = (unsigned long)tx,
+        .rx_buf = (unsigned long)rx,
+        .len = 3,
+        .speed_hz = SPI_SPEED,
+        .bits_per_word = 8,
+    };
+
+    if (ioctl(fd, SPI_IOC_MESSAGE(1), &tr) < 1) {
+        perror("SPI transfer failed");
+        return -1;
+    }
+
+    int value = ((rx[1] & 3) << 8) | rx[2];
+    return value;
+}
+
+int main() {
+    int spi_fd = open(SPI_DEVICE, O_RDWR);
+    if (spi_fd < 0) {
+        perror("Cannot open SPI device");
+        return 1;
+    }
+
+    uint8_t mode = 0;
+    ioctl(spi_fd, SPI_IOC_WR_MODE, &mode);
+    uint32_t speed = SPI_SPEED;
+    ioctl(spi_fd, SPI_IOC_WR_MAX_SPEED_HZ, &speed);
+
+    int server_fd, new_socket;
+    struct sockaddr_in address;
+    socklen_t addrlen = sizeof(address);
+
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+
+    bind(server_fd, (struct sockaddr *)&address, sizeof(address));
+    listen(server_fd, 3);
+
+    printf("ervice A waiting for connections...\n");
+
+    while (1) {
+        new_socket = accept(server_fd, (struct sockaddr *)&address, &addrlen);
+        if (new_socket >= 0) {
+            int humidity = read_channel(spi_fd, 0);
+            printf("Humidity sent: %d\n", humidity);
+            send(new_socket, &humidity, sizeof(humidity), 0);
+            close(new_socket);
+        }
+    }
+
+    close(spi_fd);
+    return 0;
+}
